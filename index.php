8ac02dc50d035ce92bad82c2cd667a96b6b9214f
John Davenport <john.davenport71@gmail.com>
2020-02-02 15:20:07 -0500

    Added attribution for chef icon

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ include('partials/main-header.php');
 ?>
   <h2>Page under construction</h2>
   <img src="assets/undraw_under_construction_46pa.png" alt="Page under construction">
-
+  <div>Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></div>
   </body>
 </html>
