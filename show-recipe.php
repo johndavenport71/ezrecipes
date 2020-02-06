14a0661583e4a61c55a21cfaea345eab5ffb6ffc
John Davenport <john.davenport71@gmail.com>
2020-02-06 10:38:57 -0500

    Getting ready to upload to web host.

--- show-recipe.php
+++ show-recipe.php
@@ -2,6 +2,7 @@
 
 include('partials/head.php');
 include('partials/main-header.php');
+include('db/query-snippets.php');
 
 $id = 9;
 
@@ -12,13 +13,16 @@ $recipe = $conn->query($sql)->fetch();
 $steps = json_decode($recipe['steps']);
 $ingredients = json_decode($recipe['ingredients']);
 
-//dd($recipe);
+$categories = getRecipeCategories($conn, $recipe['recipe_id']);
+
+dd($_SERVER['HTTP_HOST']);
 
 ?>
 
 <main>
   <h2><?= $recipe['title'] ?></h2>
   <p><?= $recipe['recipe_desc'] ?></p>
+  <h3>Ingredients</h3>
   <ul>
   <?php 
     foreach($ingredients as $name) {
@@ -26,10 +30,19 @@ $ingredients = json_decode($recipe['ingredients']);
     }
   ?>
   </ul>
+  <h3>Directions</h3>
   <ul>
     <?php
       foreach($steps as $i => $step) {
         echo "<li>Step ". ($i + 1) .": " . ucfirst($step) . "</li>";
+      }
+    ?>
+  </ul>
+  <h3>Tags</h3>
+  <ul>
+    <?php
+      foreach($categories as $cat) {
+        echo "<li>". ucfirst($cat['category_name']) ."</li>";
       }
     ?>
   </ul>
