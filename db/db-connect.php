a4cf77b34d78c5527492c328a692b843287af480
John Davenport <john.davenport71@gmail.com>
2020-02-21 20:49:00 -0500

    Cleaned up and moved files for submission.

--- db/db-connect.php
+++ db/db-connect.php
@@ -6,9 +6,6 @@ $dsn = "mysql:host=$host;dbname=$db;";
 
 try {
   $conn = new PDO($dsn, $user, $pass);
-  // if($conn) {
-  //   $control = new DBController($conn);
-  // }
 } catch (PDOException $e) {
   print($e->getMessage());
 }
