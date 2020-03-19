b564aa107bbda51ce697a85182b3525729813969
John Davenport <john.davenport71@gmail.com>
2020-03-19 13:55:31 -0400

    Removed old files

--- partials/footer.php
+++
@@ -1,5 +0,0 @@
-  <footer>
-    <div>Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></div>
-  </footer>
-  </body>
-</html>
